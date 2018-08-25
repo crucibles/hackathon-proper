b8b4a971b2885693930ab807c83bcc55a313d565
asumandang <ahsumandang@up.edu.ph>
2018-08-25 20:24:50 +0800

    finalize home page - changed bg image - added temp font - finished top-navbar

--- new-app/src/app/app.module.ts
+++ new-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CollapseModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CharacterPageComponent } from './character-page/character-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavbarComponent,
+    CharacterPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
