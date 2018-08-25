5c9359a05b96da78170158f654b15de85638ecf5
crucibles <alvaro_cedric@yahoo.com>
2018-08-25 19:54:49 +0800

    added httpclients and formgroups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import {
   BrowserAnimationsModule
 } from '@angular/platform-browser/animations';
 import { CustomOption } from './toast-option';
+import { UserService } from './shared/services/user.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -23,9 +26,12 @@ import { CustomOption } from './toast-option';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     ToastModule.forRoot()
   ],
   providers: [
+    UserService,
     { provide: ToastOptions, useClass: CustomOption }
   ],
   bootstrap: [AppComponent]
